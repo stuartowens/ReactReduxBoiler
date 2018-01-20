38027f0e04c2a7bdb0211bbd6a7c160ad7a3cb1e
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-19 21:41:06 -0600

    almost done

--- src/App.js
+++ src/App.js
@@ -8,4 +8,4 @@ export function App(props) {
       </div>)
   }
 
-export defualt App;
+export default App;
--- src/index.js
+++ src/index.js
@@ -7,9 +7,4 @@ import './index.css';
 
 // const store = configureStore();
 
-ReactDOM.render(
-  // <Provider store={ store }>
-    <App />
-  // </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
